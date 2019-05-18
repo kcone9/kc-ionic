62880e499fae7d5c45a4de0d94b14f9886bd6b51
kcone <1220678166@qq.com>
2019-05-18 11:18:42 +0800

    2we

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'index', loadChildren: './index/index.module#IndexPageModule' }
+  { path: 'index', loadChildren: './index/index.module#IndexPageModule' },
+
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  { path: 'tab5', loadChildren: './tab5/tab5.module#Tab5PageModule' }
 ];
 @NgModule({
   imports: [
