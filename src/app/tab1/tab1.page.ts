7333dd9a809ecef3f7976abb8fe561dc873ac766
kcone <1220678166@qq.com>
2019-05-20 17:51:43 +0800

    234

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-
+// import {LoadingController} from "ionic-angular"
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  user="kcone"
+  sli={
+    speed:3000,
+    initalSlide:6
+  }
+}
