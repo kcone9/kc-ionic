51c287e2b82378b45745a20928fd4dc219fbe1b2
kcone <1220678166@qq.com>
2019-05-22 16:56:07 +0800

    23

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-// import {LoadingController} from "ionic-angular"
+import {HttpClient} from "@angular/common/http"
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor(private http:HttpClient){}
   user="kcone"
-  sli={
-    speed:3000,
-    initalSlide:6
+  scroll1 = []
+  ngOnInit(){
+    this.http.get("http://127.0.0.1:5050/ionic/scroll1").subscribe((res:any)=>{
+      var row=res.reg
+    for(var i of row){
+      i.more=false
+    }
+    row.push({price:"MORE",offer:"查看更多",more:true})
+    // var n=parseInt(row.length)-1
+    // row[n].more=true
+    // console.log(row)
+    this.scroll1=row
+    })
   }
+  mo=true
 }
