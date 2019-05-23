4f27e36c1c79dfcfec9b05d56ad9689a37f86ed5
kcone <1220678166@qq.com>
2019-05-23 13:07:01 +0800

    345

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
+// import { runInThisContext } from 'vm';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,6 +10,10 @@ export class Tab1Page {
   constructor(private http:HttpClient){}
   user="kcone"
   scroll1 = []
+   h:any="2"
+   m:any="0";
+   s:any="0"
+  time=null
   ngOnInit(){
     this.http.get("http://127.0.0.1:5050/ionic/scroll1").subscribe((res:any)=>{
       var row=res.reg
@@ -16,11 +21,42 @@ export class Tab1Page {
       i.more=false
     }
     row.push({price:"MORE",offer:"查看更多",more:true})
-    // var n=parseInt(row.length)-1
-    // row[n].more=true
-    // console.log(row)
     this.scroll1=row
     })
+    this.time=setInterval(()=>{
+      this.s=parseInt(this.s)
+      this.m=parseInt(this.m)
+      this.h=parseInt(this.h)
+      this.s=this.s-1
+      if(this.s<1){
+        this.s=59
+        if(this.m>0 || this.h>0){
+          this.m=this.m-1
+          if(this.m<1){
+            this.m=59
+            this.h=this.h-1
+            if(this.h<1){
+              clearInterval(this.h)
+              this.time=null
+              this.s=this.m=this.s=0
+            }
+          }
+        }else{
+          clearInterval(this.h)
+          this.time=null
+          this.s=this.m=this.s=0
+        }
+      }
+      if(this.s<10){
+        this.s="0"+this.s
+      }
+      if(this.m<10){
+        this.m="0"+this.m
+      }
+      if(this.h<10){
+        this.h="0"+this.h
+      }
+    },1000)
   }
   mo=true
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http"
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  constructor(private http: HttpClient) { }
+  list = []
+  list1=[]
+  list2=[]
+  list3=[]
+  ngOnInit() {
+    this.http.get("http://127.0.0.1:5050/ionic/nav").subscribe((res: any) => {
+      console.log(res)
+      this.list = res
+    })
+    this.http.get("http://127.0.0.1:5050/ionic/rec?list=1").subscribe((res: any) => {
+      this.list1=res
+    })
+    this.http.get("http://127.0.0.1:5050/ionic/rec?list=2").subscribe((res: any) => {
+    this.list2=res
+    })
+    this.http.get("http://127.0.0.1:5050/ionic/rec?list=3").subscribe((res: any) => {
+    this.list3=res
+    })
+
+  }
+}
