4f7889a47d86621b696226eee4a66061ca9a2ecc
kcone <1220678166@qq.com>
2019-05-26 13:43:02 +0800

    232

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from "@angular/common/http"
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(private http:HttpClient){}
+  lapp=[]
+  ngOnInit(){
+    this.http.get("http://127.0.0.1:5050/ionic/lapp?list=1").subscribe((res:any)=>{
+      console.log(res)
+      this.lapp=res
+    })
+  }
+}
