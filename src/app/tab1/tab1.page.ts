2ed8058c17875f195ffbf4f3b7ae08a43a2fddf6
kcone <1220678166@qq.com>
2019-06-01 13:25:29 +0800

    233

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
 import {NavController} from "@ionic/angular"
 import {Router} from "@angular/router"
-// import { runInThisContext } from 'vm';
+import {IonInfiniteScroll} from "@ionic/angular"
 import {IndexPage} from "../index/index.page"
 @Component({
   selector: 'app-tab1',
@@ -10,6 +10,7 @@ import {IndexPage} from "../index/index.page"
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonInfiniteScroll) infinitScroll:IonInfiniteScroll
   constructor(private http:HttpClient,public navCtrl:NavController,private router:Router){}
   user="kcone"
   scroll = []
@@ -17,12 +18,21 @@ export class Tab1Page {
    m:any="0";
    s:any="0"
   time=null
+  text=[0,1,2,3,4,5,6,7,8,9,10]
   car=[]
   cars=[]
   carall=[]
   phone=[]
   phones=[]
   phoneall=[]
+  more=[]
+  page=1
+  gift=[{src:"https://cimgs1.fenqile.com/ibanner2/M00/32/C4/jqgHAFw1nImAH2kHAAAvT0Uh_3Y719_164x248.jpg"},
+  {src:"https://cimgs1.fenqile.com/ibanner2/M00/31/FB/jagHAFr6zluAb1M4AACD67G3dmU674_164x248.jpg"},
+  {src:"https://cimgs1.fenqile.com/ibanner2/M00/01/30/j6gHAFw1nMGATszfAACXwveIJI0986_164x248.jpg"},
+  {src:"https://cimgs1.fenqile.com/ibanner2/M00/32/C4/jqgHAFw1nMuAEJ8YAACWsmkLUOY880_164x248.jpg"}]
+  gifts=[{src:"https://cimgs1.fenqile.com/ibanner2/M00/26/6C/jqgHAFq6CASAF8QIAAKgp1ky5F0058_280x280.jpg",title:"【夏季活力版】京东E卡（800元）",pro:"京东E卡，给家人、朋友最好的爱",date:"58.44",dates:"x18期",img:"//cres1.fenqile.cn/fenqile_m/img/channel/i_like_before--053b2e8bb0.png",liked:"940人喜欢",work:"已售40件"},
+  {src:"https://cimgs1.fenqile.com/ibanner2/M00/00/55/jagHAFpEXk6ATsfCAAEi4OMZHTg294_280x280.jpg",title:"【甜蜜告白礼盒】T400天鹅施华洛锆石项链套装",pro:"百搭经典，送礼佳选",date:"14.22",dates:"x24期",img:"//cres1.fenqile.cn/fenqile_m/img/channel/i_like_before--053b2e8bb0.png",liked:"722人喜欢",work:"已售29件"}]
   search(e){
     this.router.navigate(["/index"])
   }
@@ -51,6 +61,33 @@ export class Tab1Page {
       }
     }
     this.phones=row
+  }
+  loaddata(e){
+    console.log(233)
+    setTimeout(()=>{
+      this.page=this.page+1
+      console.log(this.page)
+      this.http.get("http://127.0.0.1:5050/ionic/rec?list=14&page="+this.page).subscribe((res:any)=>{
+        if(res!=[]){
+          console.log("有",res)
+          this.more=this.more.concat(res)
+          console.log(this.more)
+          e.target.complete()
+        }else{
+          console.log("无")
+          e.target.disabled=true
+        }
+      })
+    },1000)
+  }
+  textload(e){
+    console.log("测试已触发")
+    var n=this.text.length
+    for(var i=n;i<n-1+10;i++){
+      this.text.push(i)
+    }
+    e.target.complete()
+    console.log(this.text)
   }
   ngOnInit(){
     this.http.get("http://127.0.0.1:5050/ionic/scroll").subscribe((res:any)=>{
@@ -95,8 +132,10 @@ export class Tab1Page {
         }
       }
       this.phoneall=res.s
-      console.log(row)
       this.phones=row
+    })
+    this.http.get("http://127.0.0.1:5050/ionic/rec?list=14&page="+this.page).subscribe((res:any)=>{
+      this.more=res
     })
     this.time=setInterval(()=>{
       this.s=parseInt(this.s)
